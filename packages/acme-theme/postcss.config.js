d34f9c9d59c1782d99d1704337dd0a135ea4e073
YanHui <yanhui@growingio.com>
2022-08-19 09:40:38 +0800

    fix

---
+++ packages/acme-theme/postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-mixins': {},
+    'tailwindcss/nesting': {},
+    'postcss-nested': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
--- packages/acme-theme/tailwind.config.js
+++ packages/acme-theme/tailwind.config.js
@@ -1,28 +1,36 @@
 module.exports = {
   theme: {
     colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      'theme-default': '#3867F4',
+      'theme-processing': '#0458E5',
+      'theme-success': '#00C674',
+      'theme-danger': '#F5384D',
+      'theme-warning': '#FF8A39',
+      'theme-info': '#BEBEBE',
+      'bg-default': '#ffffff',
+      'bg-primary': '#EDF0F5',
+      'bg-primary-light': '#F5F7FA',
+      'bg-disable': '#F5F7F9',
+      'border-primary': '#C5C8CF',
+      'border-primary-light': '#EEEEEE',
+      'border-disable': '#E1E2E5',
+      'text-1': '#0E0E0F',
+      'text-2': '#676870',
+      'text-3': '#A4A4A4',
+      'text-disable': '#BEBEBE',
+      'row-hover': '#EEF2F6'
     },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+    fontSize: {
+      'size-12': ['12px', '18px'],
+      'size-13': ['13px', '20px'],
+      'size-14': ['14px', '22px'],
+      'size-16': ['16px', '24px'],
+      'size-20': ['20px', '30px'],
+      'size-24': ['24px', '36px'],
+      'size-30': ['30px', '46px'],
+      'size-32': ['32px', '48px'],
     },
     extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
     }
   }
 }
